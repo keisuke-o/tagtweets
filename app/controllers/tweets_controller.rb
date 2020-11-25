fe8b4136f166d33e58b2f4f5b8769a8c87bf4748
keisuke-o <qiyoudaze5@gmail.com>
2020-11-25 20:22:35 +0900

    コントローラー　変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -17,6 +17,12 @@ class TweetsController < ApplicationController
     end
   end
 
+  def search
+    return nil if params[:input] == ""
+    tag = Tag.where(['name LIKE ?', "%#{params[:input]}%"] )
+    render json:{ keyword: tag }
+  end
+
   private
 
   def tweet_params
